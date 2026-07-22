71007c731ef60b0d4c86d238f12fa4be3ffd47ac
AhmedlSaied <kledkled.17@gmail.com>
2026-07-22 20:56:36 +0300

    Q3: Read Age from User

--- G-NET-03-C#-3/Program.cs
+++ G-NET-03-C#-3/Program.cs
@@ -14,13 +14,16 @@ namespace CSharpAssignment3
             // Output: 9
             // Explanation: Casting double to int removes the fraction part (.99) without rounding.
             #endregion
-
             #region Q2: Integer Division Fix
             // Fix: Cast one of the operands to double so it performs floating-point division
             int n = 5;
             double d2 = (double)n / 2;
             Console.WriteLine(d2);
             // Output: 2.5
+            #endregion
+            #region Q3: Read Age from User
+            Console.Write("Enter your age: ");
+            int age = int.Parse(Console.ReadLine() ?? "0");
             #endregion
         }
     }
