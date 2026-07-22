e0fcc741d69bb1b144d2a9114e31bc94456dbbf3
AhmedlSaied <kledkled.17@gmail.com>
2026-07-22 20:44:22 +0300

    Q2: Integer Division Fix

--- G-NET-03-C#-3/Program.cs
+++ G-NET-03-C#-3/Program.cs
@@ -15,7 +15,13 @@ namespace CSharpAssignment3
             // Explanation: Casting double to int removes the fraction part (.99) without rounding.
             #endregion
 
-      
+            #region Q2: Integer Division Fix
+            // Fix: Cast one of the operands to double so it performs floating-point division
+            int n = 5;
+            double d2 = (double)n / 2;
+            Console.WriteLine(d2);
+            // Output: 2.5
+            #endregion
         }
     }
 }
