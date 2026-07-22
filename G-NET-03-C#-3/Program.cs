35402331883e7789b8dce7263c5ac00026418a3e
AhmedlSaied <kledkled.17@gmail.com>
2026-07-22 20:43:19 +0300

    Q1: Casting double to int

--- G-NET-03-C#-3/Program.cs
+++ G-NET-03-C#-3/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using System;
+
+namespace CSharpAssignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1: Casting double to int
+            // Explicit casting truncates decimal part
+            double d = 9.99;
+            int x = (int)d;
+            Console.WriteLine(x);
+            // Output: 9
+            // Explanation: Casting double to int removes the fraction part (.99) without rounding.
+            #endregion
+
+      
+        }
+    }
+}
